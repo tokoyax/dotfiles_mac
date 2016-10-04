f72273b281dc0700862912a8d637adcf9d0dc4f1
tokoyax <getting.foolish@gmail.com>
2016-10-04 12:54:12 +0900

    add tmux.conf

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,7 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ctags ~/.ctags
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 mkdir ~/.vim/tmp && chmod 777 $_
