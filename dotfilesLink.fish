89c4c49605f127ad396334d252a9a68758b8dad7
tokoyax <tokoyax.dev@gmail.com>
2018-05-11 16:49:18 +0900

    add .haskeline
    ghciをvikey bindingで操作できるように

--- dotfilesLink.fish
+++ dotfilesLink.fish
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ctags ~/.ctags
 ln -sf ~/dotfiles/.ghci ~/.ghci
+ln -sf ~/dotfiles/.haskeline ~/.haskeline
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
