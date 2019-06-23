a329088eb89c927a0bb6caf3b0accc5fa43c0f81
tokoyax <tokoyax.dev@gmail.com>
2019-06-23 23:09:09 +0900

    toml導入

--- dotfilesLink.fish
+++ dotfilesLink.fish
@@ -10,16 +10,28 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.globalrc ~/.globalrc
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+
 mkdir ~/.config/fish/functions/
 ln -sf ~/dotfiles/.config/fish/functions/peco_change_directory.fish ~/.config/fish/functions/peco_change_directory.fish
 ln -sf ~/dotfiles/.config/fish/functions/peco_select_history.fish ~/.config/fish/functions/peco_select_history.fish
 ln -sf ~/dotfiles/.config/fish/functions/peco_switch_tmux_session.fish ~/.config/fish/functions/peco_switch_tmux_session.fish
+
 mkdir ~/.config/fish/completions/
 ln -sf ~/dotfiles/.config/fish/completions/hub.fish ~/.config/fish/completions/hub.fish
+
 mkdir ~/.config/alacritty
 ln -sf ~/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 mkdir ~/.config/peco
 ln -sf ~/dotfiles/.config/peco/config.json ~/.config/peco/config.json
+
+mkdir ~/.config/nvim
+mkdir ~/.config/nvim/dein
+mkdir ~/.config/nvim/dein/toml
+ln -sf ~/dotfiles/.config/nvim/dein/toml/dein.toml ~/.config/nvim/dein/toml/dein.toml
+ln -sf ~/dotfiles/.config/nvim/dein/toml/dein_linux.toml ~/.config/nvim/dein/toml/dein_linux.toml
+ln -sf ~/dotfiles/.config/nvim/dein/toml/dein_lazy.toml ~/.config/nvim/dein/toml/dein_lazy.toml
+
 mkdir ~/my_bin
 ln -sf ~/dotfiles/my_bin/delete_git_file ~/my_bin/delete_git_file
 
